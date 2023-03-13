d2b83e61966982927909a8fc649f46bb2207b793
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-13 21:35:08 +0000

    check that prompt functions work as expected

--- index.js
+++ index.js
@@ -11,18 +11,26 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
 
 
+
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+async function get_details(question) {
+    await inquirer.prompt(question).then((ans) => {
+        console.log(ans);
+    });
+}
+
+async function build_team() {
 const manager_details = [
     {name: 'name', message: 'Enter manager\'s name >>> ', type: 'input'},
     {name: 'id', message: 'Enter manager\'s employee id >>> ', type: 'input'},
     {name: 'officeNum', message: 'Enter the manager\'s office number >>> ', type: 'input'},]
-inquirer.prompt(manager_details).then((ans) => {
+ await inquirer.prompt(manager_details).then((ans) => {
     console.log(ans.name);
     console.log(ans.id);
     console.log(ans.officeNum);
 })
 
-const team = [{name: 'team', message: '', type: 'list', choices: ['Add an engineer', 'Add an intern', 'Finish Building the team']}];
+const team = [{name: 'team', message: 'Build your team', type: 'list', choices: ['Add an engineer', 'Add an intern', 'Finish Building the team']}];
 let choice = '';
 const engineer_details = [
     {name: 'name', message: 'Enter engineer\'s name >>> ', type: 'input'},
@@ -38,17 +46,18 @@ const intern_details = [
     {name: 'userName', message: 'Enter the intern\'s school', type: 'input'},
 ]
 while (choice !== 'Finish Building the team') {
-    inquirer.prompt(team).then((ans) => {
-        if (ans.team === 'Add an engineer') {
-            inquirer.prompt(engineer_details).then((ans) => {
-                console.log(ans);
-            })
-        }
-        else if (ans.team === 'Add an intern') {
-            inquirer.prompt(intern_details).then((ans) => {
-                console.log(ans);
-            })
-        }
-        choice = ans.team;
-    })
+    const ans = await inquirer.prompt(team);
+    console.log(ans)
+    if (ans.team === 'Add an engineer') {
+        const eng_ans = await inquirer.prompt(engineer_details);
+        console.log(eng_ans)
+    }
+    else if (ans.team === 'Add an intern') {
+        const int_ans = await inquirer.prompt(intern_details);
+        console.log(int_ans)
+    }
+    choice = ans.team;
 }
+}
+
+build_team();
