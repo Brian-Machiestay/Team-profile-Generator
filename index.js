d6d58d6f2d50d7a73f5094ab2d1f783dda652279
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-13 20:31:14 +0000

    Ensure node runs scripts without errors

--- index.js
+++ index.js
@@ -12,4 +12,14 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+manager_details = [
+    {name: 'name', message: 'Enter manager\'s name >>> ', type: 'input'},
+    {name: 'id', message: 'Enter manager\'s employee id >>> ', type: 'input'},
+    {name: 'officeNum', message: 'Enter the manager\'s office number >>> ', type: 'input'},]
+inquirer.prompt(manager_details).then((ans) => {
+    console.log(ans.name);
+    console.log(ans.id);
+    console.log(ans.officeNum);
+})
+
 
